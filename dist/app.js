51765e4a9fa02dd5fd5141d23d42742306d0e50f
Willzin1 <william.mendonca34@gmail.com>
2025-03-10 16:12:19 -0300

    Feat(cors): add cors

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,9 @@ var _path = require('path');
 require('./database');
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+// import helmet from 'helmet'; DEIXAR COMENTADO POIS NÃO ESTOU UTILIZANDO HTTPS, ESTOU SEM DOMÍNIO
+
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
 var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
@@ -11,6 +14,27 @@ var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interop
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
 
 _dotenv2.default.config();
+
+/**
+ * DEIXAR COMENTADO POIS É NECESSÁRIO. SERÃO AS OPTIONS QUE IRÃO NO CORS, CASO SEJA NECESSÁRIO
+ * "BLOQUEAR" ALGUNS DOMÍNIOS. Somente esses domínios da whiteList que poderão acessar a minha api.
+ * Irei deixar comentado pois quero que qualquer domínio possa acessar essa api como forma de
+ * aprendizado. Caso eu queira utilizar essas opções basta eu passar para dentro do cors.
+ *
+ * const whiteList = [
+ *   'http://35.198.12.207:81',
+ *   'http://localhost:4444',
+ * ];
+ * const corsOptions = {
+ *  origin(origin, callback) {
+ *    if (whiteList.indexOf(origin) !== -1 || !origin) {
+ *      callback(null, true);
+ *    } else {
+ *      callback(new Error('Not allowed by CORS'));
+ *    }
+ *  },
+ * };
+ */
 
 class App {
   constructor() {
@@ -20,6 +44,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, ));
+    // this.app.use(helmet()); DEIXAR COMENTADO POIS NÃO ESTOU UTILIZANDO HTTPS, ESTOU SEM DOMÍNIO
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
