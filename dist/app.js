d80da0a4bcad9bce55538a30774651b899ce6df8
Willzin1 <william.mendonca34@gmail.com>
2025-03-25 19:11:41 -0300

    Changed delay

--- dist/app.js
+++ dist/app.js
@@ -47,7 +47,7 @@ class App {
   middlewares() {
     this.app.use(_cors2.default.call(void 0, ));
     // this.app.use(helmet()); DEIXAR COMENTADO POIS NÃO ESTOU UTILIZANDO HTTPS, ESTOU SEM DOMÍNIO
-    this.app.use(_expressdelay2.default.call(void 0, 2000));
+    this.app.use(_expressdelay2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
